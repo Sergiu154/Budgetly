a1daead4753818dc239967912eb43ee51f1d0cf2
Sergiu <sergiu.victor890@gmail.com>
2020-05-18 21:55:07 +0300

    Ajustat sume pentru logica aplicatiei

--- app/src/main/java/com/example/myapplication/PieDataFragment.java
+++ app/src/main/java/com/example/myapplication/PieDataFragment.java
@@ -130,7 +130,6 @@ public class PieDataFragment extends Fragment {
         // pentru a mentine aceleasi culori la fiecare rulare, trebuie puse ordonate dupa cum le citesc
 
 
-
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
         userID = fAuth.getCurrentUser().getUid();
@@ -153,21 +152,26 @@ public class PieDataFragment extends Fragment {
                 total_spend[0] = 0f;
                 for (int i = 0; i < names.size(); ++i) {
                     TransactionDetails temp = names.get(i);
-                    total_spend[0] += (float) temp.getAmount();
-                    if(!chart_data.containsKey(temp.getCategory())) {
-                        // nu am mai avut categ asta pana acum, o adaug
-                        chart_data.put(temp.getCategory(), (float) temp.getAmount());
-                        ordered_color_set.add(known_colors.get(temp.getCategory()));
-                    }
-                    else {
-                        // daca am mai avut categoria respectiva fac suma
+                    // am salvat platile cu - in fata, ca atunci cand adaugi un income sa vine cu plus
+                    // le afisam in piechart doar pe cele cu minus, adica expenses
 
-                        double old_value = chart_data.get(temp.getCategory());
-                        double new_value = old_value + temp.getAmount();
+                    if (temp.getAmount() < 0) {
+                        // am bagat minusuri la toate in fata ca sa iasa cu plus
+                        total_spend[0] += (float) -temp.getAmount();
+                        if (!chart_data.containsKey(temp.getCategory())) {
+                            // nu am mai avut categ asta pana acum, o adaug
+                            chart_data.put(temp.getCategory(), (float) (-temp.getAmount()));
+                            ordered_color_set.add(known_colors.get(temp.getCategory()));
+                        } else {
+                            // daca am mai avut categoria respectiva fac suma
 
-                        // n-am folosit replace pt ca aparent trebuie API24 pentru aia si avem 21
-                        chart_data.remove(temp.getCategory());
-                        chart_data.put(temp.getCategory(), (float)new_value);
+                            double old_value = chart_data.get(temp.getCategory());
+                            double new_value = old_value + temp.getAmount();
+
+                            // n-am folosit replace pt ca aparent trebuie API24 pentru aia si avem 21
+                            chart_data.remove(temp.getCategory());
+                            chart_data.put(temp.getCategory(), (float) new_value);
+                        }
                     }
                 }
 
@@ -179,7 +183,7 @@ public class PieDataFragment extends Fragment {
 
                 List<PieEntry> entries = new ArrayList<>();
                 Set<String> keys = chart_data.keySet();
-                for(String key : keys) {
+                for (String key : keys) {
                     float value = chart_data.get(key);
                     PieEntry entry = new PieEntry(value, key);
 
@@ -209,13 +213,12 @@ public class PieDataFragment extends Fragment {
                 graficuMen.invalidate();
 
 
-
                 // AICI ESTE PT BAR_CHART
 
                 List<BarEntry> bar_entries = new ArrayList<>();
 
                 float poz = 0f;
-                for(String key : keys) {
+                for (String key : keys) {
                     float value = chart_data.get(key);
                     BarEntry entry = new BarEntry(value, poz);
                     poz += 1f;
@@ -230,16 +233,8 @@ public class PieDataFragment extends Fragment {
                 liniileMen.invalidate();
 
 
-
-
-
-
-
             }
         });
-
-
-
 
 
         return view;
